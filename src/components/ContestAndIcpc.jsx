21769eb6cb42f881d2c21a0ac8d78a784e1623b7
hojae <ghwo336@naver.com>
2024-10-06 00:45:48 +0900

    10/6 ContestAndIcpc develop

--- src/components/ContestAndIcpc.jsx
+++ src/components/ContestAndIcpc.jsx
@@ -1,6 +1,7 @@
 import OTC from "./../assets/OrganizingTheContest.png";
 import "./ContestAndIcpc.css";
 import React, { useState } from "react";
+import TB from "./../assets/Polygon 1.png";
 
 const ContestAndIcpc = () => {
   const [showPage, setShowPage] = useState(false);
@@ -26,12 +27,16 @@ const ContestAndIcpc = () => {
         </div>
         <div>
           <button onClick={togglePage} className="toggleButton1">
-            {showPage ? "Hide Page" : "Show Page"}
+            <img src={TB} className="button_img"></img>대회 몰아보기
           </button>
           {showPage && (
             <div className="hiddenpage">
-              <h1>동환이바보</h1>
-              <p>왕바보</p>
+              <div>- 제 1회 하이콘</div>
+              <div> - 2022 홍익대학교 HI-ARC 프로그래밍 경진대회 </div>
+              <div>-2021 홍익대학교 프로그래밍 경진대회</div>
+              <div>- 2019 홍익대학교 컴퓨터공학과 코딩대회</div>
+              <div>- 2018 홍익대학교 컴퓨터공학과 코딩대회</div>
+              <div> - 2017 홍익대학교 컴퓨터공학과 코딩대회</div>
             </div>
           )}
         </div>
@@ -43,12 +48,16 @@ const ContestAndIcpc = () => {
         </div>
         <div>
           <button onClick={togglePage2} className="toggleButton2">
-            {showPage2 ? "Hide Page" : "Show Page"}
+            <img src={TB} className="button_img"></img>ICPC 신촌 자세히 알아보기
           </button>
           {showPage2 && (
             <div className="hiddenpage2">
-              <h1>동환이바보</h1>
-              <p>왕바보</p>
+              ICPC Sinchon는 매번 방학 기간을 이용하여 연합 스터디와 합동 캠프,
+              여러 대면 행사, 프로그래밍 대회 SUAPC 등을 진행하고 있습니다. 캠프
+              강사분들의 이야기를 들으며 알고리즘에 대한 궁금증을 해소할 수 있는
+              알고리즘 토크쇼와 같은 행사를 제공합니다. 이외에도 Discord 등을
+              통하여 다양한 정보와 지식을 함께 교류하는 알고리즘 커뮤니티를
+              생성에 힘쓰고 있습니다.
             </div>
           )}
         </div>
