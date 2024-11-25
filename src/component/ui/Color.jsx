95b8a722ff6d850489b04ac00fb11f909fef6bec
hojae <ghwo336@naver.com>
2024-11-26 00:51:27 +0900

    refactor: AwardsPage-Winner 구조 개선

--- src/component/ui/Color.jsx
+++ src/component/ui/Color.jsx
@@ -9,7 +9,7 @@ const Color = {
   white: "#FFFFFF",
   black: "#000000",
   transparent: "transparent",
-  toggleColor: "#E2F5FF", //색 추가했음 수상경력페이지에 토글되는 색
+  toggleColor: "#E2F5FF4D", //색 추가했음 수상경력페이지에 토글되는 색
 };
 
 export default Color;
